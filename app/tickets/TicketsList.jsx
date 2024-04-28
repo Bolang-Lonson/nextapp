732b481b36a74c01ce3613d06ad54be1db65f569
Bolang-Lonson <bbeinyoh12@gmail.com>
2024-04-28 16:03:16 +0100

    implemented a tickets page and created a json database with ticket data which we fetch and populate the view

---
+++ app/tickets/TicketsList.jsx
@@ -0,0 +1,25 @@
+const getTickets = async () => {
+    const resp = await fetch('http://localhost:4000/tickets')
+    return resp.json()
+}
+
+export default async function TicketList() {
+
+    const tickets = await getTickets()
+    return (
+        <>
+            {tickets.map(ticket => (
+                <div key={ticket.id} className="card my-5">
+                    <h3>{ticket.title}</h3>
+                    <p>{ticket.body.slice(0, 200)}...</p>
+                    <div className={`pill ${tickets.priority}`}>
+                        {ticket.priority } priority
+                    </div>
+                </div>
+            ))}
+            {tickets.length === 0 && (
+                <p className="text-center">There are no open tickets, yay!</p>
+            )}
+        </>
+    )
+}
